f799e29baf7f0cce6a563c79c02c7f786f2d492f
Vitor Lazzaretti <vitor.lazzaretti.dev@gmail.com>
2023-02-05 17:12:56 -0300

    production

--- firebase.ts
+++ firebase.ts
@@ -5,12 +5,12 @@ import 'firebase/auth';
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAB_SmMEy00wR-L-zjop9-GS6JF5rLl6gw",
-  authDomain: "next-whatsapp-project.firebaseapp.com",
-  projectId: "next-whatsapp-project",
-  storageBucket: "next-whatsapp-project.appspot.com",
-  messagingSenderId: "1050648049603",
-  appId: "1:1050648049603:web:99bae9fbda8b59cb71af76"
+  apiKey: process.env.NEXT_PUBLIC_API_KEY || "AIzaSyAB_SmMEy00wR-L-zjop9-GS6JF5rLl6gw",
+  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN ||  "next-whatsapp-project.firebaseapp.com",
+  projectId: process.env.NEXT_PUBLIC_PROJECT_ID ||  "next-whatsapp-project",
+  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET ||  "next-whatsapp-project.appspot.com",
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGE_SENDER_ID ||  "1050648049603",
+  appId: process.env.NEXT_PUBLIC_APP_ID ||  "1:1050648049603:web:99bae9fbda8b59cb71af76"
 };
 
 const app =  getApps().length < 1 ? initializeApp(firebaseConfig) : getApp();
