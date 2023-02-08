87fee65fc5c54d77d6b76f4d89384deaa86182b0
Vitor Lazzaretti <vitor.lazzaretti.dev@gmail.com>
2023-02-08 15:19:30 -0300

    Bug Fix Try 03

--- components/ChatItem.tsx
+++ components/ChatItem.tsx
@@ -2,8 +2,8 @@ import { Avatar } from '@mui/material';
 import { User } from 'firebase/auth';
 import { collection, getDocs, query, QuerySnapshot, where } from 'firebase/firestore';
 import moment from 'moment';
-import Link from 'next/link';
-import React, { useEffect, useState } from 'react'
+import { useRouter } from 'next/router';
+import React, { useEffect, useState } from 'react';
 import { db } from '../firebase';
 import getRecipientEmail from '../utils/getRecipientEmail';
 
@@ -22,6 +22,7 @@ type UserInfo = {
 };
 
 const ChatItem = ({ id, users, loggedUser, lastSent, selected, notify, onClick }: Props) => {
+  const router = useRouter();
   const [recipientUser, setRecipientUser] = useState<UserInfo>();
   const recipientEmail = getRecipientEmail(users, loggedUser);
   const recipientUserCollection = collection(db, "users");
@@ -35,10 +36,10 @@ const ChatItem = ({ id, users, loggedUser, lastSent, selected, notify, onClick }
   }, []);
 
   return (
-    <Link
-      href={`/chat/${id}`}
+    <div
+
       className='flex items-center cursor-pointer justify-between break-words ml-[1px] h-20 bg-neutral-900 bg shadow-inner shadow-neutral-700 hover:bg-neutral-800'
-      onClick={() => onClick(id)}
+      onClick={() => { onClick(id); router.push(`/chat/${id}`); }}
     >
       <div className='flex items-center h-full'>
         <div className={`${selected ? 'bg-yellow-500' : 'bg-neutral-500'} h-full w-1`}></div>
@@ -55,7 +56,7 @@ const ChatItem = ({ id, users, loggedUser, lastSent, selected, notify, onClick }
         <p className={`text-[12px] mb-2 text-gray-300 ${notify ? 'text-yellow-500' : 'text-gray-300'}`}> {lastSent ? moment(lastSent).format('LT') : 'Unavailable'} </p>
         <div className={`w-5 h-5 ${notify ? 'bg-yellow-500' : 'bg-transparent'} rounded-full`}></div>
       </div>
-    </Link>
+    </div>
   )
 }
 
